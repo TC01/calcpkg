d14aa1e36d7b56a44da9e93fcfdedfecaefbaad1
unknown <rosser.bjr@gmail.com>
2012-09-13 23:00:25 -0400

    If setup.py is ran on Windows, the script will be created as 'calcpkg.py'

--- setup.py
+++ setup.py
@@ -2,6 +2,20 @@
 
 from distutils.core import setup
 
+import os
+import shutil
+import sys
+
+#This is really bad, but hey... it works!
+scriptname = "calcpkg"
+if sys.platform == "win32":
+	try:
+		print "Creating script as calcpkg.py because we are running Windows\n"
+		shutil.copy("calcpkg", "calcpkg.py")
+	except:
+		pass	#We are not making a distribution, we are installing one or doing some other operation
+	scriptname = "calcpkg.py"
+
 setup(	name = "calcpkg",
 		version = "2.0",
 		description = "ticalc.org package manager",
@@ -11,5 +25,13 @@ setup(	name = "calcpkg",
 		author_email = "rosser.bjr@gmail.com",
 		url = "http://www.ticalc.org/archives/files/fileinfo/433/43348.html",
 		packages = ["calcrepo"],
-		scripts = ["calcpkg"],
+		scripts = [scriptname],
 		package_data={'calcrepo': ['ticalc.files.index', 'ticalc.names.index']} )
+
+#Clean up the mess we made
+try:
+	if sys.platform == "win32":
+		os.remove("calcpkg.py")
+except:
+	pass
+	
