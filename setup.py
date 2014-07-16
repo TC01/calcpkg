1f2307e5727bf7f6124d9d6469d9485c2e8c9719
Ben Rosser <rosser.bjr@gmail.com>
2014-07-16 17:55:47 -0400

    Moved calcpkg 'script' inside calcrepo library, we're using setuptools entrypoints magic now.

--- setup.py
+++ setup.py
@@ -62,7 +62,6 @@ setup(
 	# simple. Or you can use find_packages().
 #	packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
 	packages=['calcrepo', 'calcrepo.repos'],
-	py_modules=['bin/calcpkg'],
 
 	# List run-time dependencies here.  These will be installed by pip when your
 	# project is installed. For an analysis of "install_requires" vs pip's
@@ -88,7 +87,7 @@ setup(
 	# pip to create the appropriate form of executable for the target platform.
 	entry_points={
 		'console_scripts': [
-			'calcpkg=calcpkg:main',
+			'calcpkg=calcrepo.calcpkg:main',
 		],
 	},
 	)
